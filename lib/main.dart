fd83efbbfff9633c54707167a09875d15f9bc728
kf0830s <kf0830s@gmail.com>
2021-10-24 15:11:32 +0900

    Add:main.dart初期設定追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "シンプルすぎる計算脳トレ",
+      theme: ThemeData.dark(),
+      home: HomeScreen(),
+    );
+  }
+}
