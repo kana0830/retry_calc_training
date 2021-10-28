2309d2cecc049db0a4601c17db5050cb9989f667
kf0830s <kf0830s@gmail.com>
2021-10-28 20:25:32 +0900

    Add:bodyの大枠追加

--- lib/screens/test_screen.dart
+++ lib/screens/test_screen.dart
@@ -12,10 +12,38 @@ class TestScreen extends StatefulWidget {
 class _TestScreenState extends State<TestScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(widget.numberOfQuestions.toString()),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            //スコア表示部分
+            _scorePart(),
+            //問題表示部分
+            _questionPart(),
+            //電卓ボタン部分
+            _calcButtons(),
+            //答えボタン部分
+            _answerCheckbutton(),
+            //戻るボタン
+            _buckButton(),
+          ],
+        ),
       ),
     );
   }
+
+  //TODO スコア表示部分
+  _scorePart() {}
+
+  //TODO 問題表示部分
+  _questionPart() {}
+
+  //TODO 電卓ボタン部分
+  _calcButtons() {}
+
+  //TODO 答え合わせボタン
+  _answerCheckbutton() {}
+
+  //TODO 戻るボタン
+  _buckButton() {}
 }
